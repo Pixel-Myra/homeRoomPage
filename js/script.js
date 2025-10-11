c3cb8ba1b36a4f5a73750b3544710161730ba0df
Pixel-Myra <ragnaris2020@gmail.com>
2025-10-11 14:20:55 +0300

    added a "help" and "kitchen" button

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@
 let rooms = JSON.parse(localStorage.getItem('rooms')) || [];
 rooms.push({name: "Моя кімната", items: []});
 localStorage.setItem('rooms', JSON.stringify(rooms));
+function showPopup() {
+  alert("Cam rosberesa 😊");
+}
+function openPopup() {
+    document.getElementById('popupOverlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+}
+function openPopup(name) {
+  document.getElementById('popup-' + name).style.display = 'block';
+}
+
+function closePopup(name) {
+  document.getElementById('popup-' + name).style.display = 'none';
+}
